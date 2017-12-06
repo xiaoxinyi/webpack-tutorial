3bf51effea092f06228e118719918b52d2619281
Zhang Li <xiaoxin88214@163.com>
2017-12-06 23:02:55 +0800

    implement 03 webpack dev server

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
+// 把css当作module引入到js module中
+import '../css/main.css'
+import '../css/input-elements.css'
+// 使用webpack时，需要告诉webpack导入了哪些包
+// 所以不需要考虑js的导入顺序了
+import {secretButton, secretParagraph} from "./dom-loader";
+
+'./dom-loader'
+
 var showSecret = false;
 
 secretButton.addEventListener('click', toggleSecretState);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// node.js 内置包
+var path = require('path')
+// web pack包
+var webpack = require('webpack')
+
+
+module.exports = {
+    // 定义js入口文件
+    entry: './src/js/app.js',
+    output: {
+        // 最后输出的路径
+        path: path.resolve(__dirname, 'dist'),
+        // 最后输出的整个js文件
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    // 这里定义了webpack会怎样对待你的module
+    // 那些import都可以看成是module，css文件也可以看成是module
+    module: {
+      rules: [
+          // 每一条rule都是一个javascript对象
+          {
+              // rule作用于的文件
+              test: /\.css$/,
+              use: [
+                  // 有顺序要求，从底向上进行导入
+                  // webpack 执行逆向执行loader
+                  // loader用来translate javascript code
+                  'style-loader',
+                  'css-loader'
+              ]
+          }
+      ]
+    },
+    // plugins作用于最后的生成的bundle.js
+    // npm install webpack-dev-server@~2.7.0 --save-dev
+    // 有版本问题 要降低版本
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            // ...
+        })
+    ]
+
+};
