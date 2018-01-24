b306ba0ae5f65d67301c8f3e9c67ad8706138828
Zhang Li <xiaoxin88214@163.com>
2018-01-24 14:26:34 +0800

    done

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        // 
+        // 相对index.html的路径
         // publicPath: '/dist'
     },
     module: {
